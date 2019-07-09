22e8c48f6c9387ba5aeb5042e70c69e6c131d637
Learner <linhdqtg@gmail.com>
2019-07-10 03:20:59 +0700

    New nation

---
+++ src/config/Ascii.h
@@ -0,0 +1,48 @@
+// Copyright (c) 2018, The TurtleCoin Developers
+// Copyright (c) 2018, The DeroGold Association
+//
+// Please see the included LICENSE file for more information
+
+#pragma once
+
+const std::string asciiArt =
+"                             ___=sivvuoXXXXXoouas_s,,                             \n"
+"                       ..=ssvuSXXS2SXXZZZZZ######BBBmwwa>..                       \n"
+"                    __vunoonooo2S2SSSXXZZZ#Z#####mmmm###m#ma,,.                   \n"
+"                ..iaoXSoooooo2222S2SSXXZZ#Z######mmmmmm######moc..                \n"
+"              __wXXXXS22222o2o2SoS2XSSXXXXZZUU##mmmBmmm####UZUZZXo,,              \n"
+"            _ao#ZXXXXSSS222oooo1**}+++~~~~++'!*?YSX##m#####Z#ZZXZXZoi.            \n"
+"          _aZZUZZXZXXXSS221}'+--                  -^'!YX#UZZZZXXXXXS2oi.          \n"
+"        .vd#Z#ZZZZZZXXXe>~-                            -~*SXZXXXXSo2o2nv>         \n"
+"       <dXZZZZZ#Z#ZZX*~-                                  --*3SSoo2ooonnvi,       \n"
+"     .vXXZZZZZZZZZX}'                                        -+1o2onnnvvvvi;      \n"
+"    .uSXXXZXZZZUZ*'                ..___<_=__..                -|InvnvvvvvvI=     \n"
+"   .uSSSXXXXZXZ2^              ._=vvnoXXZ#mmmmwoa,,              :{vvIvIIIIvI=    \n"
+"   vS2S2SSSXXXe'            .=aooXooSSSXZU#######mps,..           -+lvIIvIvIIv;   \n"
+"  )ooo2222S2S}'           .vo#XXXSoo2oSXZZ##mm###ZXXZo>.            +vIIIvvIvvs.  \n"
+" .onnoooo2o21'           <wZX11**|*|*{***Y?YSU##ZZXXoons,            +vvvvvvvvv>  \n"
+" )nvvvnnnnn1'          .vdXZXoas=,          --'*SSoonnvvi;          .=vvvvvvnvnv: \n"
+".vIIvIvlIvv>           voSXXXXZZ#c              -<1vvIllll;  .___|ivvvvvnvnnnnoo> \n"
+":vIIlIllIli'          =vnnooSSXXXc                +iilillIi|ivivnnvnvnnnnoooooo2c.\n"
+"=vIIllllll|           |IlIIvvvvnX>                 +iiiillIIvlvvvnvnnnnnnnonoo22o.\n"
+"=liii|i|||=          .|+|++|+|||+=                 :||||+++++++|++|+|+|{nnnnoo2oo:\n"
+"=iiii|i|i|;           =++|+|=|=||:                 :|+|>.        .     =nvono2ooo;\n"
+":iIlllllll=           =llvlvvnnno=                 ||ii|.             .<noooo2o2e:\n"
+" {vIIIIlll>.          :{ono22SSXZ>                |illii:             .)onooooo21.\n"
+".<IIvIvvIvv;           <XSXXXXZZUi              _%nvvIIi;             =vnnnnnoooi \n"
+" =vvvvvvnnns.           )XXZZUZ#Z(         . __uXSoonnvvv+           .ivvnvvnvnv= \n"
+" .{nnnoooo22s.           +SX#ZZXXosiiiissaaawqm#ZXXX2on}+           .<vvvvvvvvv}  \n"
+"  =no2S2SSSS2s.           -)SXXXX2oooS2XXZ##mmmm##ZXX1>'           .<vIvIvIvvvl;  \n"
+"   <S2SSSSSSXXs.            -+*Xooo2SoXXZ###mmBm##X1+-            .%lIIIIIvIII+   \n"
+"    {SXSSXXXXXXo=.             -~+**12SSXX#X#XXS!^-              =vvvvIvIIIIv>    \n"
+"     {XXXXXZZZZ#Xa,                 ----++~----                _vvnvvvvvvvIv>     \n"
+"      )XXZZZZZZZZZqs,.                                      ._%oooonnnvvvvv+      \n"
+"       +3XZZZZ#Z#ZZXXs,,                                  __udSS222oonnnn}~       \n"
+"        -{XZ#ZZZZZZZXXXos,_.                          ._=awZZXXSSS2Sooon(         \n"
+"          -?ZUZZZZXXXXXXSSns=__.                  ._ssum##ZZZZXXXXS2SS}'          \n"
+"            ~*SZXXXXXXS22ooooSoussi>=_______iisauwqm#####Z##ZUZZXXX2}-            \n"
+"              -'XXXXXS2So2oo2oo222SSXXZmXmq##Z##mmmmmmm#####Z#ZZZX''              \n"
+"                --*1So222oo2o222S2SSXXXXZZZ#Z###mmmmmmmm#####ZY!--                \n"
+"                    -'*noo2o222S2S2XXXZZZZ#Z#####mmBmmmmm##e'^                    \n"
+"                        -+!!*12oS2XXXXXZUZ#U#####mmmUTYY'-                        \n"
+"                             --~^+|{{I1S1X2211*!!'''-                             \n";
